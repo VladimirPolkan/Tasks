979bf01f7056eb6787621780cb6ccf8e1844b629
VladimirPolkan <r.plus.vladimir@gmail.com>
2021-03-16 01:18:29 +0300

    beautiful solution

---
+++ Data.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+//echo "Hello World";
+$a=date("d,m,y" );
+echo "Текущая дата: $a<br/>\n";
+$tm=date("h,i,s" );
+echo "Текущее время: $tm<br/>\n";
+?>
+<p><a href="Домашняя страница.html">Вернутся обратно</a></p>
+</body>
+</html>
---
+++ Squaring and cube.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<h1>Возведение в квадрат и куб</h1>
+
+    <?php
+
+    for($i=1;$i<=5;$i++) {
+    echo "<li>$i  в квадрате равно=".($i*$i);
+    echo ",  $i  в кубе равно=".($i*$i*$i)."</li>\n";
+}
+?>
+
+<p><a href="Домашняя страница.html">Вернутся обратно</a></p>
+</body>
+</html>
