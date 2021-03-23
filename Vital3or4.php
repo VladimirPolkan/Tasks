8571f52e613e7735a5c4dc21cfa2b5f661c5b248
VladimirPolkan <r.plus.vladimir@gmail.com>
2021-03-23 18:05:16 +0300

    Vita_ne_smog_schitat'_ydalennuj_massiv

--- Vital3or4.php
+++ Vital3or4.php
@@ -15,16 +15,35 @@
     <a href="https://www.youtube.com/watch?v=ZmsXBwRy9DM&t=334s&ab_channel=ITDoctor">Чтение из файла и запись в файл на
         PHP</a>
 </p>
+<p>
+    <a href="https:/https://qna.habr.com/q/555109">Как составить строку из значений нескольких переменных</a>
+</p>
 <?php
-$Hoepage = file_get_contents('input.txt');
-for ($a = 0, $j = 1, $b = 0; $j <= $Hoepage; $j++) {
-    if ($j % 2 == 0) {
+// $array = file_get_contents('input.txt');
+//echo '<pre>';
+//print_r($array);
+//echo '</pre>';
+
+//$array = [5, 4, 16, 19, 31, 2];
+$array = [8, 29, 4, 7, 12, 15, 17, 24, 1];
+$s1 = "";
+$s2 = "";
+
+for ($a = 0, $b = 0, $j = 1; $j <= $array[0]; $j++) {
+    if ($array[$j] % 2 == 0) {
         $b++;
+        $s2 = join(" ", [$s2, strval($array[$j])]);
+
     } else {
         $a++;
+        $s1 = join(" ", [$s1, strval($array[$j])]);
+
     }
-    echo "j=$j a=$a b=$b <br/>\n";
 }
+
+echo "$s1<br/>";
+echo "$s2<br/>";
+
 if ($a > $b) {
     echo "Витя получит 3";
 } elseif ($a < $b) {
@@ -32,6 +51,7 @@ if ($a > $b) {
 } else {
     echo "У Вити 4, но с натяжкой";
 }
+echo ".\" <br/>\n Всего $array[0], из них \"3\".. $a, а  \"4\"..$b ";
 ?>
 <p><a href="/">Вернутся обратно</a></p>
 </body>
